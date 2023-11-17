207ff10ccbd09a79c8a2a8b4d26976815dc0b8ad
Rizky Faisal Rafi <87520408+RizkyFaisalRafi@users.noreply.github.com>
2023-11-17 21:16:46 +0700

    Add Models EmploteData

---
+++ lib/models/employee_data.dart
@@ -0,0 +1,29 @@
+class EmployeeData {
+  final String? alamat;
+  final String? divisi;
+  final String? email;
+  final String? jabatan;
+  final String? jenisKelamin;
+  final String? name;
+  final String? nip;
+  final String? numberPhone;
+  final String? password;
+  final String? sisaCuti;
+  final String? tanggalLahir;
+  final String? uid;
+
+  EmployeeData({
+    required this.alamat,
+    required this.divisi,
+    required this.email,
+    required this.jabatan,
+    required this.jenisKelamin,
+    required this.name,
+    required this.nip,
+    required this.numberPhone,
+    required this.password,
+    required this.sisaCuti,
+    required this.tanggalLahir,
+    required this.uid,
+  });
+}
