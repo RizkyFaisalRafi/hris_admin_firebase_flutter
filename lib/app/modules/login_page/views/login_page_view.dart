0b3c2453667ce24c1229832dd546151f0288c9f7
Rizky Faisal Rafi <87520408+RizkyFaisalRafi@users.noreply.github.com>
2023-11-17 16:30:11 +0700

    Update login_page_view.dart
    Update Widget UI

--- lib/app/modules/login_page/views/login_page_view.dart
+++ lib/app/modules/login_page/views/login_page_view.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 import 'package:hris_admin_firebase_flutter/widgets/button_custom.dart';
 import 'package:hris_admin_firebase_flutter/widgets/custom_text_field.dart';
 
+import '../../../../common/theme.dart';
 import '../controllers/login_page_controller.dart';
 
 class LoginPageView extends GetView<LoginPageController> {
@@ -15,53 +16,87 @@ class LoginPageView extends GetView<LoginPageController> {
           title: const Text('Login Admin'),
           centerTitle: true,
         ),
-        body: Form(
-          key: controller.formKey,
-          child: ListView(
-            padding: const EdgeInsets.all(20),
-            children: [
-              // Header
-              Image.asset(
-                'assets/logo_app.png',
-                width: 200,
-                height: 200,
-              ),
-              // Email
-              CustomTextField(
-                obscure: false,
-                controller: controller.emailC,
-                validate: controller.validateEmail,
-                hintText: 'Email',
-                typeForm: TextInputType.emailAddress,
-                isShow: false,
-                togleObscure: controller.toggleObscureText,
-                imageAsset: 'assets/icon_email.png',
-              ),
+        body: Center(
+          child: SingleChildScrollView(
+            child: Form(
+              key: controller.formKey,
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  // mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // Header
+                    Image.asset(
+                      'assets/logo_app.png',
+                      width: 220,
+                      height: 220,
+                    ),
+                    const SizedBox(height: 28),
 
-              // Password
-              Obx(() => CustomTextField(
-                    obscure: controller.obscureText.value,
-                    controller: controller.passC,
-                    validate: controller.validatePass,
-                    hintText: 'Your Password',
-                    typeForm: TextInputType.visiblePassword,
-                    isShow: true,
-                    togleObscure: controller.toggleObscureText,
-                    imageAsset: 'assets/icon_password.png',
-                  )),
-              const SizedBox(
-                height: 32,
+                    Text(
+                      'Sign In Admin',
+                      style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: medium,
+                      ),
+                    ),
+
+                    const SizedBox(height: 20),
+
+                    // Email
+                    CustomTextField(
+                      obscure: false,
+                      controller: controller.emailC,
+                      validate: controller.validateEmail,
+                      hintText: 'Email',
+                      typeForm: TextInputType.emailAddress,
+                      isShow: false,
+                      togleObscure: controller.toggleObscureText,
+                      imageAsset: 'assets/icon_email.png',
+                    ),
+
+                    const SizedBox(height: 12),
+
+                    // Password
+                    Obx(
+                      () => CustomTextField(
+                        obscure: controller.obscureText.value,
+                        controller: controller.passC,
+                        validate: controller.validatePass,
+                        hintText: 'Your Password',
+                        typeForm: TextInputType.visiblePassword,
+                        isShow: true,
+                        togleObscure: controller.toggleObscureText,
+                        imageAsset: 'assets/icon_password.png',
+                      ),
+                    ),
+                    const SizedBox(height: 32),
+                    ButtonCustom(
+                      label: 'Masuk',
+                      onTap: () async {
+                        if (controller.formKey.currentState!.validate()) {
+                          await controller.login();
+                        }
+                      },
+                      isExpand: true,
+                    ),
+
+                    Container(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {
+                          // provider.resetPassword(context);
+                        },
+                        child: const Text(
+                          'Lupa Password',
+                          style: TextStyle(color: Colors.red),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              ButtonCustom(
-                label: 'Masuk',
-                onTap: () async{
-                  if (controller.formKey.currentState!.validate()) {
-                    await controller.login();
-                  }
-                },
-                isExpand: true,
-              )
-            ],
+            ),
           ),
         ));
   }
